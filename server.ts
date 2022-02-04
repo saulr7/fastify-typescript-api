33f1e7ba415b5cc311de8bf2df23afd6b3a4efd3
Saul Ramirez <fernando.ramire@hotmail.com>
2022-02-04 13:28:31 -0600

    :rotating_light: fixing eslint warnings

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import App from './app';
 class Server {
   private app: App;
 
+  // eslint-disable-next-line no-unused-vars
   constructor(public port: number) {
     this.app = new App();
   }
@@ -10,8 +11,10 @@ class Server {
   async serve() {
     try {
       const add = await this.app.build().listen(this.port);
+      // eslint-disable-next-line no-console
       console.log('running at', add);
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.log('Something went wrong', error);
     }
   }
